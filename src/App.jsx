97d8d4c1414f279d169c81dc76e74cf564dfe183
Rafael Andriotti Rebelo <rafael.rebelo@GUAIBA.LOCAL>
2026-08-20 16:57:01 -0300

    Atualizando background do site

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from './components/sections/Hero'
 import Projects from './components/sections/Projects'
 import Resume from './components/sections/Resume'
 import Skills from './components/sections/Skills'
-import Aurora from './components/Aurora'
 import CursorNamePrompt from './components/CursorNamePrompt'
 import CustomCursor from './components/CustomCursor'
 import Footer from './components/Footer'
@@ -20,7 +19,6 @@ function App() {
 
   return (
     <div className="site-shell">
-      <Aurora />
       <CustomCursor />
       <CursorNamePrompt />
       <a className="skip-link" href="#conteudo-principal">
